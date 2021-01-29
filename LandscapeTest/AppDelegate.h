7efe479c94d3eb83e16ec66f6b1627fd0abcfb61
冯星 <fengxing2@bytedance.com>
2021-01-29 17:29:02 +0800

    Initial Commit

---
+++ LandscapeTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LandscapeTest
+//
+//  Created by annidy on 2021/1/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
